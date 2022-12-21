0bf6473e2403aaeb23cd34b3a432b9a06220b34a
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-12-21 20:25:03 +0300

    Решение задачи 3

--- Task_3.java
+++ Task_3.java
@@ -1,7 +1,27 @@
 package HW_Exceptions_2;
 
-public class Task_3 {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
 
+public class Task_3 {
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+            //Throwable был тут
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) { // Throwable перенесен сюда так как от низшего уровня к высшему
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
     }
 }
