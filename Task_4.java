5c203a4f73a4a1927927e68182a9d24ff95357fe
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-12-21 20:33:17 +0300

    Решение задачи 4

--- Task_4.java
+++ Task_4.java
@@ -1,7 +1,15 @@
 package HW_Exceptions_2;
 
+import java.util.Scanner;
+
 public class Task_4 {
     public static void main(String[] args) {
-
+        Scanner scann = new Scanner(System.in);
+        String text = scann.nextLine();
+        if (text.isEmpty()) {
+            throw new RuntimeException("Вводить пустые строки нельзя");
+        }
+        System.out.println(text);
+        scann.close();
     }
 }
