852e9f7e5c6bbe9be78a0970726cb1d6c2c5ef6c
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-12-21 20:14:31 +0300

    Решение задачи 2

--- Task_2.java
+++ Task_2.java
@@ -2,6 +2,15 @@ package HW_Exceptions_2;
 
 public class Task_2 {
     public static void main(String[] args) {
-
+        int[] intArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
     }
 }
+
+// Код исправлять ненужно
